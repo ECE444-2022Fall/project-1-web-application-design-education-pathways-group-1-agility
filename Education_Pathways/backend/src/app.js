4e83dd3662d9bfe439efa2ec0ce1c0fb24cfaa4c
abdullah578 <64642049+abdullah578@users.noreply.github.com>
2022-11-06 23:19:42 -0500

    Enable cors usage

--- Education_Pathways/backend/src/app.js
+++ Education_Pathways/backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 require("./db/mongoose.js");
 const courseRouter = require("./router/course");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(courseRouter);
 
 module.exports = app;

244d6ec8ebb2fb795fa12aa01f575d41a5e3abf3
Nissar Ishtiak <80916975+ishtiakn@users.noreply.github.com>
2022-11-09 21:26:39 -0500

    Finished implementing search/filter functionality

--- Education_Pathways/frontend/src/components/ResultDisplay.jsx
+++ Education_Pathways/frontend/src/components/ResultDisplay.jsx
@@ -6,18 +6,65 @@ import "./css/Result.css";
 import Label from "./Label";
 import "./css/styles.css";
 
+async function getDropdownValues(s) {
+  let dropdownValues = []
+  try{
+    let res = await axios.get('/courses/'.concat(s));
+    dropdownValues = res.data;
+  }catch(error){
+    dropdownValues = ["Error. Failed to retrieve dropdown values"];
+  }
+  return dropdownValues;
+};
+
+function arrayDictNumbers(minVal, maxVal){
+  let arr_dict = [];
+  for (var i=minVal; i<=maxVal; i++){
+    arr_dict.push({value: i.toString(), text: i.toString()})
+  };
+  return arr_dict;
+}
+
 class SearchResultDisplay extends Component {
   constructor() {
     super();
     this.state = {
+      campuses: [],
+      faculties: [],
+      departments: [],
       input: "",
+      faculty: "",
+      department: "",
+      minLevel: -1,
+      maxLevel: -1,
       dispSpinner: false,
       results: [],
     };
   }
 
+  async componentDidMount(){
+    this.setState({ dispSpinner: true});
+
+    let arr = await getDropdownValues('faculties')
+    let arr_dict = []
+    for (var i=0; i<arr.length; i++){
+      arr_dict.push({value: arr[i], text: arr[i]})
+    }
+    this.setState({ faculties: arr_dict})
+
+    arr = await getDropdownValues('departments')
+    arr_dict = []
+    for (i=0; i<arr.length; i++){
+      arr_dict.push({value: arr[i], text: arr[i]})
+    }
+    this.setState({ departments: arr_dict})
+
+    this.setState({ dispSpinner: false});
+  }
+
   handleChange = (event) => {
-    this.setState({ input: event.target.value });
+    const value = event.target.value;
+    this.setState({[event.target.name] : value });
   };
 
   handleSubmit = (event) => {
@@ -27,7 +74,27 @@ class SearchResultDisplay extends Component {
 
   getData = (input) => {
     this.setState({ dispSpinner: true});
-    axios.get(`/courses?search=${input}`).then((res) => {
+    //Construct the API Get Request
+    let getRequestURL = "/courses?search="//${input}`
+    if (this.state.input !== ""){
+      getRequestURL = getRequestURL.concat(this.state.input.replace(/&/g, "%26"));
+    }
+    if (this.state.faculty !== ""){
+      getRequestURL = getRequestURL.concat("&faculty=").concat(this.state.faculty.replace(/&/g, "%26"));
+    }
+    if (this.state.department !== ""){
+      getRequestURL = getRequestURL.concat("&department=").concat(this.state.department.replace(/&/g, "%26"));
+    }
+    if (this.state.minLevel !== ""){
+      getRequestURL = getRequestURL.concat("&minLevel=").concat(this.state.minLevel);
+    }
+    if (this.state.maxLevel !== ""){
+      getRequestURL = getRequestURL.concat("&maxLevel=").concat(this.state.maxLevel);
+    }
+    
+    console.log(getRequestURL)
+    //Call the API
+    axios.get(getRequestURL).then((res) => {
       if (res.status === 200) {
         this.setState({ results: [] });
         console.log(res.data);
@@ -55,11 +122,9 @@ class SearchResultDisplay extends Component {
         alert("System Error. Please refresh");
       }
       this.setState({dispSpinner: false});
-    }).catch(()=>{
-      this.setState({dispSpinner: false});
     });
   };
-
+  
   render() {
     return this.state.dispSpinner ? (
       <Spinner />
@@ -70,13 +135,54 @@ class SearchResultDisplay extends Component {
           <br></br>
           <form onSubmit={this.handleSubmit} className={"search"}>
             <input
-              placeholder={"Search for course code, course name, keyword ..."}
+              placeholder={"Search for courses by code, title, keyword..."}
               className={"text-input"}
               type="text"
+              name="input"
               value={this.state.input}
               onChange={this.handleChange}
             />
             <input type="submit" value="Search" className={"submit-button"} />
+            <br></br><br></br>            
+            <label for="faculty">Faculty:</label>
+            <select name="faculty" onChange={this.handleChange} value={this.state.faculty} className={"dropdown"} id="faculty">
+              <option value="" className={"dropdown"} selected="selected">any</option>
+              {this.state.faculties.map((option, index) => (
+                <option key={index} value={option.value} className={"dropdown"}>
+                  {option.text}
+                </option>
+              ))}
+            </select>
+            <br></br><br></br>
+            <label for="department">Department:</label>
+            <select name="department" onChange={this.handleChange} value={this.state.department} className={"dropdown"} id="department">
+              <option value="" className={"dropdown"} selected="selected">any</option>
+              {this.state.departments.map((option, index) => (
+                <option key={index} value={option.value} className={"dropdown"}>
+                  {option.text}
+                </option>
+              ))}
+            </select>
+            <br></br><br></br>
+            <label for="minLevel">Min. Level:</label>
+            <select name="minLevel" onChange={this.handleChange} value={this.state.minLevel} className={"dropdown"} id="minLevel">
+              <option value="" className={"dropdown"} selected="selected">none</option>
+              {arrayDictNumbers(0, 7).map((option, index) => (
+                <option key={index} value={option.value} className={"dropdown"}>
+                  {option.text}
+                </option>
+              ))}
+            </select>
+            <b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b>
+            <label for="maxLevel">Max. Level:</label>
+            <select name="maxLevel" onChange={this.handleChange} value={this.state.maxLevel} className={"dropdown"} id="maxLevel">
+              <option value="" className={"dropdown"} selected="selected">none</option>
+              {arrayDictNumbers(0, 7).map((option, index) => (
+                <option key={index} value={option.value} className={"dropdown"}>
+                  {option.text}
+                </option>
+              ))}
+            </select>
           </form>
         </div>
         <div className={"search-result-display"}>{this.state.results}</div>

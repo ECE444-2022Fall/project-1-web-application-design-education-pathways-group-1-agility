a0b0cccdbdb7abf3aceacabba846e1d20d76773b
abdullah578 <64642049+abdullah578@users.noreply.github.com>
2022-11-10 21:01:08 -0500

    Did some minor refactoring in frontend

--- Education_Pathways/frontend/src/components/SearchCompare.js
+++ Education_Pathways/frontend/src/components/SearchCompare.js
@@ -26,46 +26,59 @@ class SearchBarCompare extends Component {
   };
 
   getData = (input) => {
-    this.setState({ dispSpinner: true});
-    axios.get(`/courses?search=${input}`).then((res) => {
-      if (res.status === 200) {
-        this.setState({ results: [] });
-        console.log(res.data);
-        if (res.data.length > 0) {
-          
-          let len = res.data.length;
-          let result_temp = [];
-          result_temp.push(<Label></Label>);
-          for (let i = 0; i < len; i++) {
-            result_temp.push(
-              <ResultCompare
-                key={res.data[i]._id}
-                course_id={res.data[i]._id}
-                course_code={res.data[i].Code}
-                course_name={res.data[i].Name}
-                course_faculty={res.data[i].Faculty}
-                course_department={res.data[i].Department}
-                course_description={res.data[i]["Course Description"]}
-                course_pre={res.data[i]["Pre-requisites"]}
-
-                setCourse1={this.props.setCourse1}
-                setCourse2 = {this.props.setCourse2}
-                course1 = {this.props.course1}
-                course2 = {this.props.course2}
-              ></ResultCompare>
-            );
+    this.setState({ dispSpinner: true });
+    axios
+      .get(`/courses?search=${input}`)
+      .then((res) => {
+        if (res.status === 200) {
+          this.setState({ results: [] });
+          console.log(res.data);
+          if (res.data.length > 0) {
+            let len = res.data.length;
+            let result_temp = [];
+            result_temp.push(<Label></Label>);
+            for (let i = 0; i < len; i++) {
+              result_temp.push(
+                <ResultCompare
+                  key={res.data[i]._id}
+                  course_id={res.data[i]._id}
+                  course_code={res.data[i].Code}
+                  course_name={res.data[i].Name}
+                  course_faculty={res.data[i].Faculty}
+                  course_department={res.data[i].Department}
+                  course_description={res.data[i]["Course Description"]}
+                  course_pre={res.data[i]["Pre-requisites"]}
+                  setCourse1={this.props.setCourse1}
+                  setCourse2={this.props.setCourse2}
+                  course1={this.props.course1}
+                  course2={this.props.course2}
+                ></ResultCompare>
+              );
+            }
+            this.setState({ results: result_temp });
+          } else {
+            alert("Course not found");
           }
-          this.setState({ results: result_temp });
-        } else {
-          alert("Course not found");
+        } else if (res.status === 500) {
+          alert("System Error. Please refresh");
         }
-      } else if (res.status === 500) {
-        alert("System Error. Please refresh");
-      }
-      this.setState({dispSpinner: false});
-    }).catch(()=>{
-      this.setState({dispSpinner: false});
-    });
+        this.setState({ dispSpinner: false });
+      })
+      .catch(() => {
+        let result_temp = [];
+        result_temp.push(
+          <Result
+            key={""}
+            course_id={""}
+            course_code={"No results found."}
+            course_name={""}
+            course_faculty={""}
+            course_department={""}
+          ></Result>
+        );
+        this.setState({ results: result_temp });
+        this.setState({ dispSpinner: false });
+      });
   };
 
   render() {
@@ -73,10 +86,10 @@ class SearchBarCompare extends Component {
       <Spinner />
     ) : (
       <div className="SearchQuery">
-        <div >
+        <div>
           <form onSubmit={this.handleSubmit} className={"search"}>
             <input
-            key = {"qiao"}
+              key={"qiao"}
               placeholder={"Search for course code, course name, keyword ..."}
               className={"text-input"}
               type="text"
@@ -87,7 +100,7 @@ class SearchBarCompare extends Component {
           </form>
         </div>
         <div className={"search-result-display"}>{this.state.results}</div>
-{/*         
+        {/*         
         <button onClick={()=>{
           console.log(this.props.course1)
           console.log(this.props.course2)

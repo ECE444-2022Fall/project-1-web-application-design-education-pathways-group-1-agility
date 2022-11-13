cedb4462e6ae65b40429eddc7b219881592c551e
Nissar Ishtiak <80916975+ishtiakn@users.noreply.github.com>
2022-11-13 16:29:35 -0500

    Made search/filter warnings unclickable

--- Education_Pathways/frontend/src/components/Results.jsx
+++ Education_Pathways/frontend/src/components/Results.jsx
@@ -31,25 +31,21 @@ class Result extends Component{
     if (this.state.course_code === "NO_PARAMS_ENTERED"){
       return(
         <Container>
-          <a href={`courseDetails/${this.state.course_id}`} onClick={this.redirectCourse} className={"search-result-item"} style={{textDecoration: "none"}}>
           <Row className={"result-display"}>
               <Col>
                   <h5>Try entering a search term or applying filter.</h5>  
               </Col>
           </Row>
-          </a>
         </Container>
       )
     } else if (this.state.course_code === "NO_RESULTS_FOUND") {
       return(
         <Container>
-          <a href={`courseDetails/${this.state.course_id}`} onClick={this.redirectCourse} className={"search-result-item"} style={{textDecoration: "none"}}>
           <Row className={"result-display"}>
               <Col>
                   <h5>No courses found.</h5>  
               </Col>
           </Row>
-          </a>
         </Container>
       )
     } else { //There are search results to display

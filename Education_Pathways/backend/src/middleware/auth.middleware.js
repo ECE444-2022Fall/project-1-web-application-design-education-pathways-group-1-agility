e9d53bd50a96c9e2521ecc5da12e3e943aaa0c31
abdullah578 <64642049+abdullah578@users.noreply.github.com>
2022-11-14 16:13:29 -0500

    Minor backend refactoring

--- Education_Pathways/backend/src/middleware/auth.middleware.js
+++ Education_Pathways/backend/src/middleware/auth.middleware.js
@@ -6,8 +6,10 @@ const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
   try {
-    const token = req.header("Authorization");
-    jwt.verify(token.split(" ")[1], process.env.JWT_SECRET_KEY);
+    jwt.verify(
+      req.headers.authorization.split(" ")[1],
+      process.env.JWT_SECRET_KEY
+    );
     next();
   } catch (err) {
     res.status(401).send("Bad auth");

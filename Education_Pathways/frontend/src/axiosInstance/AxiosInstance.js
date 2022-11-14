a2c6f0fb520f9a83db5dcea13f18aea11b36a149
abdullah578 <64642049+abdullah578@users.noreply.github.com>
2022-11-13 19:35:00 -0500

    Deploy backend to heroku

--- Education_Pathways/frontend/src/axiosInstance/AxiosInstance.js
+++ Education_Pathways/frontend/src/axiosInstance/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const myInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://agility-education-pathways.herokuapp.com",
 });
 
 myInstance.interceptors.request.use(
